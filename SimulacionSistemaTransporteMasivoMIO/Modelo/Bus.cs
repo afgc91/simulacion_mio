bab337ebe0c4217d6524b266c9143d7ea085257c
afgc91 <andy-cruz18@hotmail.com>
2014-09-22 16:18:38 -0500

    Creación clases de carga de datos 22/9

---
+++ SimulacionSistemaTransporteMasivoMIO/Modelo/Bus.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulacionSistemaTransporteMasivoMIO.Modelo
+{
+    public class Bus
+    {
+        /// <summary>
+        /// Identificador de la clase bus.
+        /// </summary>
+        public int BusId;
+        
+        /// <summary>
+        /// Número del bus.
+        /// </summary>
+        public int BusNumber;
+
+        public String Identification;
+        
+        /// <summary>
+        /// Identificador del tipo de bus al cual pertenece el bus.
+        /// </summary>
+        public int BusTypeId;
+
+        /// <summary>
+        /// Identificador de la instancia de PlanVersion a la cual perternece el bus.
+        /// </summary>
+        public int PlanVersionId;
+
+        public Bus(int busId, int busNumber, String identification, int busTypeId, int planVersionId) {
+            BusId = busId;
+            BusNumber = busNumber;
+            Identification = identification;
+            BusTypeId = busTypeId;
+            PlanVersionId = planVersionId;
+        }
+    }
+}
---
+++ SimulacionSistemaTransporteMasivoMIO/Modelo/BusType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulacionSistemaTransporteMasivoMIO.Modelo
+{
+    public class BusType
+    {
+        /// <summary>
+        /// Identificador de la clase BusTypeId.
+        /// </summary>
+        public int BusTypeId;
+
+        public DateTime ShortName;
+        public DateTime LongName;
+
+        public BusType(int busTypeId, DateTime shortName, DateTime longName) {
+            BusTypeId = busTypeId;
+            ShortName = shortName;
+            LongName = longName;
+        }
+    }
+}
