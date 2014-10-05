397ff3de27dd84874e571beefb1a9e1d09e81821
afgc91 <andy-cruz18@hotmail.com>
2014-10-05 15:56:56 -0500

    Creación VentanaMIO

---
+++ SimulacionSistemaTransporteMasivoMIO/Vista/VentanaMIO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimulacionSistemaTransporteMasivoMIO.Vista
+{
+    public partial class VentanaMIO : Form
+    {
+        public VentanaMIO()
+        {
+            InitializeComponent();
+        }
+    }
+}
