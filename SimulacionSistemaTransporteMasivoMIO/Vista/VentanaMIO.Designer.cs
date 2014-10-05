1341d9b24ec741c939ba45b4d7c56cebb0e9d17a
afgc91 <andy-cruz18@hotmail.com>
2014-10-05 18:01:47 -0500

    Ventana MIO

--- SimulacionSistemaTransporteMasivoMIO/Vista/VentanaMIO.Designer.cs
+++ SimulacionSistemaTransporteMasivoMIO/Vista/VentanaMIO.Designer.cs
@@ -28,19 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.gMapMIO = new GMap.NET.WindowsForms.GMapControl();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.gMapMIO);
+            this.splitContainer1.Size = new System.Drawing.Size(1200, 630);
+            this.splitContainer1.SplitterDistance = 798;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // gMapMIO
+            // 
+            this.gMapMIO.Bearing = 0F;
+            this.gMapMIO.CanDragMap = true;
+            this.gMapMIO.GrayScaleMode = false;
+            this.gMapMIO.LevelsKeepInMemmory = 5;
+            this.gMapMIO.Location = new System.Drawing.Point(12, 12);
+            this.gMapMIO.MarkersEnabled = true;
+            this.gMapMIO.MaxZoom = 20;
+            this.gMapMIO.MinZoom = 12;
+            this.gMapMIO.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gMapMIO.Name = "gMapMIO";
+            this.gMapMIO.NegativeMode = false;
+            this.gMapMIO.PolygonsEnabled = true;
+            this.gMapMIO.RetryLoadTile = 0;
+            this.gMapMIO.RoutesEnabled = true;
+            this.gMapMIO.ShowTileGridLines = false;
+            this.gMapMIO.Size = new System.Drawing.Size(775, 606);
+            this.gMapMIO.TabIndex = 0;
+            this.gMapMIO.Zoom = 12D;
             // 
             // VentanaMIO
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(442, 313);
+            this.ClientSize = new System.Drawing.Size(1200, 630);
+            this.Controls.Add(this.splitContainer1);
             this.Name = "VentanaMIO";
             this.Text = "VentanaMIO";
+            this.Load += new System.EventHandler(this.VentanaMIO_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private GMap.NET.WindowsForms.GMapControl gMapMIO;
     }
 }
--- SimulacionSistemaTransporteMasivoMIO/Vista/VentanaMIO.cs
+++ SimulacionSistemaTransporteMasivoMIO/Vista/VentanaMIO.cs
@@ -7,6 +7,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GMap;
+using GMap.NET.WindowsForms;
+using GMap.NET.ObjectModel;
+using GMap.NET.WindowsForms.Markers;
+using GMap.NET;
 
 namespace SimulacionSistemaTransporteMasivoMIO.Vista
 {
@@ -15,6 +20,21 @@ namespace SimulacionSistemaTransporteMasivoMIO.Vista
         public VentanaMIO()
         {
             InitializeComponent();
+        }
+
+        private void VentanaMIO_Load(object sender, EventArgs e)
+        {
+            gMapMIO.MapProvider = GMap.NET.MapProviders.GoogleMapProvider.Instance;
+            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerOnly;
+
+            gMapMIO.Position = new GMap.NET.PointLatLng( ﻿3.41000, -76.53333);
+
+            GMapOverlay markersOverlay = new GMapOverlay(gMapMIO, "markers");
+            GMapMarkerGoogleGreen marker = new GMapMarkerGoogleGreen(new PointLatLng(﻿3.41000, -76.53333));
+            markersOverlay.Markers.Add(marker);
+            gMapMIO.Overlays.Add(markersOverlay);
+
+            marker.ToolTipText = "Información estación: \n   Cantidad pasajeros";
         }
     }
 }
